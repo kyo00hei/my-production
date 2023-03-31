ae98ee58b0255534807a9207ee613581778e2414
kyo00hei <115925078+kyo00hei@users.noreply.github.com>
2023-03-31 15:40:27 +0900

    管理者権限機能の更新

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', '商品一覧')
 
 @section('content_header')
-    <h1>user一覧</h1>
+    <h1>管理権限</h1>
 @stop
 
 @section('content')
@@ -11,7 +11,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">一覧</h3>
+                    <h3 class="card-title">登録者一覧</h3>
                     <div class="card-tools">
 
                     </div>
@@ -23,7 +23,8 @@
                                 <th>ID</th>
                                 <th>名前</th>
                                 <th>email</th>
-                                <th>権限</th>
+                                <th>権限No</th>
+                                <th>権限付与</th>
                                 <th></th>
                                 <th></th>
                             </tr>
@@ -36,19 +37,36 @@
                                     <td>{{ $user->email }}</td>
                                     <td>{{ $user->role }}</td>
                                     <td>
-                                        <form method="POST" action="{{ route('user.update', $user->id) }}" class="user pl-5">
-                                            @method('put')
-                                            @csrf
-                                            <input type="number" class="row-g3 w-25" name="role" placeholder="role" value="{{ $user->role}}">
-                                            <button type="submit" class="btn btn-outline-info btn-sm">変更</button>
+                                        @foreach(config('role') as $role_value => $role)
+                                            @if($user->role==$role_value)
+                                                {{$role}}
+                                            @endif
+                                        @endforeach
+                                    </td>
+                                    <td>
+                                            <form method="POST" action="{{ route('user.update', $user->id) }}" class="user pl-5">
+                                                @method('put')
+                                                @csrf
+                                            @if('0' == $user->role || '20' < $user->role )
+                                                <input type="hidden" name="role" value="2">
+                                                <button type="submit" class="btn btn-outline-warning btn-sm">権限付与</button>
+                                            @elseif('1' != $user->role)
+                                                <input type="hidden" name="role" value="0">
+                                                <button type="submit" class="btn btn-warning btn-sm">付与解除</button>
+                                            @endif
+                                            <!--権限付与フォーム-->
+                                                <!--<input type="number" class="row-g3 w-25" name="role" placeholder="role" value="{{ $user->role}}">
+                                                <button type="submit" class="btn btn-outline-info btn-sm">変更</button>-->
                                         </form>
                                     </td>
                                     <td>
+                                        @if('1' != $user->role)
                                         <form action="{{ route('user.destroy' ,$user->id) }}" method="POST">
                                             @method('delete')
                                             @csrf
                                             <button type="submit" class="btn btn-danger">削除</button>
                                         </form>
+                                        @endif
                                     </td>
                                 </tr>
                             @endforeach
