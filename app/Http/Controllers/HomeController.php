a9f4ff50b9f578801bdefb7c13e86fd2a8a5f3c0
kyo00hei <115925078+kyo00hei@users.noreply.github.com>
2023-04-03 03:40:08 +0900

    履歴表示数変更

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,9 +23,13 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
+     /**
+      * 履歴表示機能
+      */
     public function index()
     {
-        $logs = Log::orderBy('created_at','DESC')->take(10)->get();
+        $logs = Log::orderBy('created_at','DESC')->take(30)->get();
 
         return view('home' ,compact('logs'));
     }
